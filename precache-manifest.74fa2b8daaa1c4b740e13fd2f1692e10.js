eb40c6751b1019f5ac8aad4d9d79d27eb3b39098
mwawrzonkowski <wawko2@gmail.com>
2020-07-07 20:12:59 +0200

    Updates

---
+++ precache-manifest.74fa2b8daaa1c4b740e13fd2f1692e10.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab0ce52900cddc6e04fe5951cb901a27",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "70e34a1f4d5e0e5c9cb3",
+    "url": "/Portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "6742ae515d3da28eb04e",
+    "url": "/Portfolio/static/css/main.47c732b5.chunk.css"
+  },
+  {
+    "revision": "70e34a1f4d5e0e5c9cb3",
+    "url": "/Portfolio/static/js/2.0ef25fbd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.0ef25fbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6742ae515d3da28eb04e",
+    "url": "/Portfolio/static/js/main.15fc56c8.chunk.js"
+  },
+  {
+    "revision": "425e952245b014fc3ffb",
+    "url": "/Portfolio/static/js/runtime-main.db605a1d.js"
+  },
+  {
+    "revision": "cd3f051a74708dc23e824e356e07bb2d",
+    "url": "/Portfolio/static/media/ActiveBox.cd3f051a.png"
+  },
+  {
+    "revision": "1d6eaa7182b0d1a162d4759f2aba7872",
+    "url": "/Portfolio/static/media/face.1d6eaa71.png"
+  },
+  {
+    "revision": "a2ddcb0a7659e529a49a51811b0d1456",
+    "url": "/Portfolio/static/media/portfolio.a2ddcb0a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.9e0441972043e13dd15ca24ad0cf3413.js"
+  "/Portfolio/precache-manifest.74fa2b8daaa1c4b740e13fd2f1692e10.js"
 );
 
 self.addEventListener('message', (event) => {
